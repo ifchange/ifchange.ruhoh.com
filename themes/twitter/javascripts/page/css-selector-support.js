b792eef32aa081f8e1d084e2fa7df2606ef45073
ifchange <netman8410@163.com>
2012-12-07 19:48:12 +0800

    some update

---
+++ themes/twitter/javascripts/page/css-selector-support.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+        $("a.iframe").fancybox({
+                'frameWidth':   820,
+                'frameHeight':  600,
+                'overlayOpacity':   0.7,
+                'hideOnContentClick':   false
+        });
+    });
